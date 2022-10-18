eeb0c77f25a30e355ba3446244d1d04ca432d94b
Arthri <41360489+a@users.noreply.github.com>
2022-10-18 01:16:45 +0000

    Add IFilter with options

---
+++ src/ExtensibleCounterspam/IFilterWithOptions.cs
@@ -0,0 +1,13 @@
+﻿namespace ExtensibleCounterspam;
+
+/// <summary>
+/// Represents a filter that accepts options.
+/// </summary>
+/// <typeparam name="TOptions"></typeparam>
+public interface IFilterWithOptions<TOptions> : IFilter
+{
+    /// <summary>
+    /// Gets the filter's configuration.
+    /// </summary>
+    TOptions Options { get; }
+}
